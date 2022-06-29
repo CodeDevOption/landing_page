e8451c1f1fdb883c294de8c54456d578746b213e
CodeDevOption <80384952+CodeDevOption@users.noreply.github.com>
2022-06-29 02:28:39 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -2,16 +2,14 @@
 const hours = Selector(".hours");
 const minutes = Selector(".minutes");
 const seconds = Selector(".seconds");
+const swich = document.getElementById('switch');
+const img = document.getElementById('img');
 setInterval(() => {
     const date = new Date();
     hours.textContent = date.getHours() < 10 ? `0${date.getHours()}`: date.getHours();
     minutes.textContent = date.getMinutes()< 10 ? `0${date.getMinutes()}`: date.getMinutes();;
     seconds.textContent = date.getSeconds()< 10 ? `0${date.getSeconds()}`: date.getSeconds();;
 }, 1000);
-
-const swich = document.getElementById('switch');
-const img = document.getElementById('img');
-
 swich.onchange = (e)=>{
     if(e.target.checked){
         console.log(img.src);
